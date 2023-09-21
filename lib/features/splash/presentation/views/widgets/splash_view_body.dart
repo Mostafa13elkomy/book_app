0cc0c9902f610b443dab8e4dab5ec1b8e838e842
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-09-21 11:55:08 +0300

    refactor init state & create navigate home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:book_app/constances.dart';
 import 'package:book_app/core/utlis/assets.dart';
-import 'package:book_app/features/presentation/views/widgets/slidingtext.dart';
+import 'package:book_app/features/home/presentaion/views/home_view.dart';
+import 'package:book_app/features/splash/presentation/views/widgets/slidingtext.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,20 +20,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
-
   @override
   void didUpdateWidget(covariant SplashViewBody oldWidget) {
     super.didUpdateWidget(oldWidget);
     animationController.dispose();
   }
-
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -43,5 +42,19 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
-}
 
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+  void navigateToHome() {
+      Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTranstionAnimation);
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:book_app/constances.dart';
-import 'package:book_app/features/presentation/views/widgets/splash_view.dart';
+import 'package:book_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
