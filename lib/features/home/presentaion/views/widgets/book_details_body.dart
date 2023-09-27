c8b6ce5156df2a96dbab9fc1f9eef94b6cac7960
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-09-27 08:42:22 +0300

    add subtitle to book details

--- lib/features/home/presentaion/views/widgets/book_details_body.dart
+++ lib/features/home/presentaion/views/widgets/book_details_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/core/utlis/styles.dart';
 import 'package:book_app/features/home/presentaion/views/widgets/custom_book_details_app_bar.dart';
 import 'package:book_app/features/home/presentaion/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +14,26 @@ class BookDetailsViewBody extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * .22),
-          child:const CustomBookImage(),
-        )
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .9,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+        ),
       ],
     );
   }
