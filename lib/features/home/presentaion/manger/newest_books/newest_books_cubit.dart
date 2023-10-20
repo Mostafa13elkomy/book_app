279bfd4346c996d89c08dad33ac72927b5271364
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-10-20 11:25:17 +0300

    newest books cubit done

--- lib/features/home/presentaion/manger/newest_books/newest_books_cubit.dart
+++ lib/features/home/presentaion/manger/newest_books/newest_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:book_app/features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+    final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
 }
